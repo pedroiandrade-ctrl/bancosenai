ce24d30649c6cb53d9dd4de586a21ff439480831
pedroiandrade-ctrl <pedro.i.andrade@aluno.senai.br>
2026-09-11 08:54:11 -0300

    feat(documento) validacao arquivo

--- DEVLAB_BACK_API/AppBack/BancoSENAIAPISLN/BancoSENAIAPI/Controllers/DocumentoController.cs
+++ DEVLAB_BACK_API/AppBack/BancoSENAIAPISLN/BancoSENAIAPI/Controllers/DocumentoController.cs
@@ -17,7 +17,10 @@ namespace BancoSENAIAPI.Controllers
         [HttpPost("upload/{codigoCliente}")]
         public async Task<IActionResult> AnexarArquivo(int codigoCliente, IFormFile arquivo)
         {
-
+            if (arquivo == null || arquivo.Length == 0) 
+            {
+                return BadRequest("Nenhum arquivo foi enviado.");
+            }
         }
     }
 }
